18e1829c14eea197d139a20b079b9526ced0d161
logbyjungle <lucamiao072@gmail.com>
2023-09-09 10:15:36 +0200

    numeri,stringhe e valori booleani su Java

--- src/jungle/Main.java
+++ src/jungle/Main.java
@@ -1,8 +1,31 @@
 package jungle;
 
+import java.security.cert.TrustAnchor;
+
 public class Main {
 
     public static void main(String[] args) {
-        //code
+        // tipi di numeri
+        int numero0 = 30;
+        // int = integer,numero intero, numero0=variabile , 30= valore , ; = finire la riga
+        float numero1 = 15.249f;
+        // float = numero decimale , f è necessario , i float hanno 7 cifre dopo la virgola di precisione
+        double numero2 = 92.23888223f;
+        double numero3 = 92.23888223d;
+        double numero4 = 92.23888223;
+        // double = numero decimale con 15 cifre di precisione dopo la virgola, si può usare sia f che d che nulla
+        byte numero5 = 100;
+        // contiene numeri da -128 a 127
+        short numero6 = 30000;
+        // contiene numeri da -32768 a 32767
+        boolean possibilità0 = true;
+        boolean possibilità1 = false;
+        // booleano ovvero vero o falso
+        char carattere0 = 'j';
+        // contiene un singolo carattere
+        String frase0 = "questa frase non esiste";
+        String frase1 = null;
+        // contiene una stringa di testo oppure niente
+
     }
 }
